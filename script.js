7cab9d7c5e5d185c3ba0aae95cc2ac8600de7003
Raphael velasco <raphaelvelasco2@gmail.com>
2022-01-20 15:01:56 -0300

    Fiz o desafio

---
+++ script.js
@@ -0,0 +1,32 @@
+let oi = "Olá"
+function mostrarIdade(){
+    console.log(oi)
+}
+
+mostrarIdade()
+
+function mostrarNome(myname){
+    console.log(`Meu nome é ${myname}`)
+}
+
+mostrarNome("Raphael")
+
+function númeroMúsicalCantor(number, musical, singer){
+    console.log(`Eu tenho ${number} anos`)
+    console.log(`Gosto de ${musical}`)
+    console.log(`Meu cantor favorito é o ${singer}`)
+}
+
+númeroMúsicalCantor(17, "Pagode", "Xande de Pilares")
+ 
+function músicaFilme(music, movie){
+    console.log(`Minha música favorita é ${music} do Grupo Revelacão`)
+    console.log(`E meu filme favorito é ${movie}`)
+}
+músicaFilme("Deixa acontecer", "Shrek para Sempre")
+
+function número(number2){
+        return number2 * 3
+}
+
+console.log(número(9))
